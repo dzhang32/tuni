d850c2c32de6208d30a552f4737f059ea13c3dbb
David Zhang <32676710+dzhang32@users.noreply.github.com>
2024-05-23 11:03:45 +0100

    docs: annotate errors

--- src/error.rs
+++ src/error.rs
@@ -1,34 +1,41 @@
+//! Custom error types returned by tuni.
+
 use std::path::PathBuf;
 use thiserror::Error;
 
+/// Errors resulting from cli parsing.
 #[allow(clippy::enum_variant_names)]
 #[derive(Error, Debug)]
 pub enum CliError {
+    /// The file provided is unreadable.
     #[error("FileReadError: Unable to read file {0:?}")]
     FileReadError(PathBuf),
-
+    /// The GTF provided is not a file or does not have a ".gtf" extension.
     #[error("GtfParseError: GTFs must be a file with the '.gtf' extension, found {0:?}")]
     GtfParseError(PathBuf),
-
+    /// The path does not point to a directory (e.g. it is a file).
     #[error("NotADirectoryError: output_dir must be an existing directory {0:?}")]
     NotADirectoryError(PathBuf),
 }
 
+/// Errors resulting from processing GTF lines.
 #[allow(clippy::enum_variant_names)]
 #[derive(Error, Debug)]
 pub enum GtfError {
+    /// The ("exon" or "CDS") record does not contain the "transcript_id" attribute.
     #[error("MissingTranscriptIdError: No transcript_id found in line {0:?}")]
     MissingTranscriptIdError(String),
-
+    /// `tuni` should filter for only "exon"/"CDS" records. Therefore, if this
+    /// error appears, it likely points to a tuni bug in filtering.
     #[error("UnknownFeatureError: Feature must be 'exon' or 'CDS', found {0:?}.")]
     UnknownFeatureError(String),
-
+    /// The line from the GTF could not be read.
     #[error("LineReadError: Unable to read line in {0:?}")]
     LineReadError(PathBuf),
-
+    /// The file could not be created.
     #[error("FileCreateError: Unable to create output file {0:?}")]
     FileCreateError(PathBuf),
-
+    /// Could not write to the file.
     #[error("FileWriteError: Unable to write line to {0:?}")]
     FileWriteError(PathBuf),
 }
