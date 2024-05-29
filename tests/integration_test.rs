9073b0ec38dc002d198ce9b5f1f9bd194e9529d5
David Zhang <32676710+dzhang32@users.noreply.github.com>
2024-05-29 19:36:38 +0100

    test: include gffs in integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,33 +1,53 @@
 use assert_cmd::Command;
 use predicates::prelude::predicate;
+use rstest::rstest;
 use std::fs::read_to_string;
 use tempfile::tempdir;
 
-#[test]
-fn test_tuni() {
+#[rstest]
+#[case("tests/data/integration/gtf_paths.txt", "gtf")]
+#[case("tests/data/integration/gff_paths.txt", "gff")]
+fn test_tuni(#[case] gtf_gff_path: &str, #[case] gtf_gff_extension: &str) {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     let temp_dir = tempdir().unwrap();
 
     // RUST_LOG=INFO env var is used to ensure env_logger stores logs to the stderr.
     cmd.env("RUST_LOG", "INFO")
         .arg("--gtf-gff-path")
-        .arg("tests/data/integration/gtf_paths.txt")
+        .arg(gtf_gff_path)
         .arg("--output-dir")
         .arg(temp_dir.path());
 
-    let check_warning_message =
-        predicate::str::contains("Unrecognised transcript ID found transcript_id \"F\"");
+    let check_warning_message = predicate::str::contains("Unrecognised transcript ID found");
 
     cmd.assert().success().stderr(check_warning_message);
 
     // Check assigned unified IDs are correct.
     // For descriptions of test cases, see test_case attribute in input gtfs.
     assert_eq!(
-        read_to_string("tests/data/integration/expected_sample_1.tuni.gtf").unwrap(),
-        read_to_string(temp_dir.path().join("sample_1.tuni.gtf")).unwrap(),
+        read_to_string(format!(
+            "tests/data/integration/expected_sample_1.tuni.{}",
+            gtf_gff_extension
+        ))
+        .unwrap(),
+        read_to_string(
+            temp_dir
+                .path()
+                .join(format!("sample_1.tuni.{}", gtf_gff_extension))
+        )
+        .unwrap(),
     );
     assert_eq!(
-        read_to_string("tests/data/integration/expected_sample_2.tuni.gtf").unwrap(),
-        read_to_string(temp_dir.path().join("sample_2.tuni.gtf")).unwrap(),
+        read_to_string(format!(
+            "tests/data/integration/expected_sample_2.tuni.{}",
+            gtf_gff_extension
+        ))
+        .unwrap(),
+        read_to_string(
+            temp_dir
+                .path()
+                .join(format!("sample_2.tuni.{}", gtf_gff_extension))
+        )
+        .unwrap(),
     );
 }
